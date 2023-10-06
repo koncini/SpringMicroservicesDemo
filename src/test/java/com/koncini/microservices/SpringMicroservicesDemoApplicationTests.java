7f1358ecdbae552e575e6c714b1cd37303f68751
koncini <juan.rincon8205@gmail.com>
2023-10-06 14:44:06 -0500

    First Commit

---
+++ src/test/java/com/koncini/microservices/SpringMicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koncini.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
