4034a22cc960d221ebe2c5e1febc58e590f70765
koncini <juan.rincon8205@gmail.com>
2023-10-09 22:24:26 -0500

    Creating Order Service

---
+++ OrderService/src/test/java/com/koncini/microservices/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koncini.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
