06ee39d8c7dd95dfa7263bb820fb9a62c22bb3c0
koncini <juan.rincon8205@gmail.com>
2023-10-12 13:16:03 -0500

    Implementing Api Gateway

---
+++ ApiGateway/src/test/java/com/koncini/microservices/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koncini.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
